c4c30dfba6267b1802dfee5e7f39ded18157b57a
gyq <yqgong@pku.edu.cn>
2018-04-12 01:17:42 +0800

    "vimcpp"

---
+++ viminit2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#apt-cyg install ctags
+#ctags -R --sort=yes --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++
+
+#cd ~/.vim/bundle && wget "http://www.vim.org/scripts/download_script.php?src_id=19574" -O taglist.zip && unzip taglist.zip -d taglist
